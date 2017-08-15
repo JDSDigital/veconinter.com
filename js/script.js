c59c2dd9d0542725aa46b79e1d4a17e95821aa6b
Daniel Sosa <jdsosa@gmail.com>
2017-08-15 15:50:22 -0400

    Modify blog and press sections

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ $(function () {
         pauseOnHover: true
     });
 
-    var headerImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg'];
+    var headerImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg'];
     var randomAll = Math.floor(Math.random() * headerImages.length);
     var randomMain = Math.floor(Math.random() * 2);
 
