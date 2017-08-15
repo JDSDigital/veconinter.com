9f571c52e29242c63e001f6432b460310ee2879f
Daniel Sosa <jdsosa@gmail.com>
2017-08-14 22:07:59 -0400

    Merge Changes

--- js/script.js
+++ js/script.js
@@ -32,10 +32,11 @@ $(function () {
     });
 
     var headerImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg'];
-    var num = Math.floor(Math.random() * headerImages.length);
+    var randomAll = Math.floor(Math.random() * headerImages.length);
+    var randomMain = Math.floor(Math.random() * 2);
 
-    $('.main-slider').parallax({imageSrc: './img/header/full/' + headerImages[num]});
-    $('.services-slider').parallax({imageSrc: './img/header/full/' + headerImages[num]});
+    $('.main-slider').parallax({imageSrc: './img/header/full/' + headerImages[randomMain]});
+    $('.services-slider').parallax({imageSrc: './img/header/full/' + headerImages[randomAll]});
     //$('.values-row').parallax({imageSrc: './img/03.jpg'});
     $('.send-resume-block').parallax({imageSrc: './img/team2.jpg'});
 });
