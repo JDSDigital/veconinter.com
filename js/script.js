adeb38e240af3e03481110d2c65dd3c25effeec1
Daniel Sosa <jdsosa@gmail.com>
2017-08-16 10:15:16 -0400

    Small changes

--- js/script.js
+++ js/script.js
@@ -1,47 +1,29 @@
-/*
-jQuery('#rev_slider_1').show().revolution({
- 
-    sliderType: 'carousel',
- 
-    carousel: {
- 
-        maxVisibleItems: 5,
-        space: 10,
-        infinity: 'on',
-        stretch: 'off',
- 
-        border_radius: '10px',
-        horizontal_align: 'center',
- 
-        fadeout: 'off',
-        vary_fade: 'off',
- 
-        vary_rotation: 'off',
-        maxRotation: 0,
- 
-        vary_scale: 'off',
-        minScale: 50
- 
-    },
-});*/
 $(function () {
+    /**
+     * Top marquee with exchange rates
+     */
     $('.marquee').marquee({
         duration: 20000,
         duplicated: true,
         pauseOnHover: true
     });
 
+    /**
+     * Image change in top header
+     *
+     * @type {string[]}
+     */
     var headerImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg'];
     var randomAll = Math.floor(Math.random() * headerImages.length);
     var randomMain = Math.floor(Math.random() * 2);
 
     $('.main-slider').parallax({imageSrc: './img/header/full/' + headerImages[randomMain]});
     $('.services-slider').parallax({imageSrc: './img/header/full/' + headerImages[randomAll]});
-    //$('.values-row').parallax({imageSrc: './img/03.jpg'});
     $('.send-resume-block').parallax({imageSrc: './img/team2.jpg'});
-});
 
-$(document).ready(function() {
+    /**
+     * Top nav bar opacity change on scroll
+     */
     $(window).scroll(function() {
         if ($(this).scrollTop() > 0) {
             $('#navigation_bar').css('opacity', 1);
@@ -50,6 +32,9 @@ $(document).ready(function() {
         }
     });
 
+    /**
+     * Image changer on About Us accordion
+     */
     $('#goal').on('click', function() {
         $('#about-image').html('<div class="row text-center">' +
             '<img class="img-responsive m0a" src="./img/logo-spinner.png" alt="Veconinter ISO 9001" />' +
