45ad758911d12fab98867e4a720b116ebe61bdb8
Daniel Sosa <jdsosa@gmail.com>
2017-09-17 16:22:44 -0400

    Modified VecoAccess Landing Page

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ $(function () {
     $('.values-row').parallax({imageSrc: './img/values.jpg'});
     $('.banner-vecoaccess').parallax({imageSrc: './img/type-away-vecoaccess.jpg'});
     $('.landing-slider').parallax({imageSrc: './img/slider-vecoaccess.jpg'});
+    $('.landing-container').parallax({imageSrc: './img/header/full/01.jpg'});
 
     /**
      * Top nav bar opacity change on scroll
