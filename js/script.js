36e74808dc7cb47cafdaa5e093f5b2df197f716b
Daniel Sosa <jdsosa@gmail.com>
2017-11-06 20:56:23 -0400

    Modify Landing 1 and 3

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(function () {
     $('.landing-container').parallax({imageSrc: './img/header/full/01.jpg'});
     $('.services-parallax').parallax({imageSrc: './img/containers.jpg'});
     $('.counters_services').parallax({imageSrc: './img/containers.jpg'});
-    $('.landing-slider-3').parallax({imageSrc: './img/user-app-1.jpg'});
+    $('.landing-slider-3').parallax({imageSrc: './img/containers.jpg'});
     //$('.landing-slider-3-2').parallax({imageSrc: './img/user-app-2.jpg'});
 
     /**
